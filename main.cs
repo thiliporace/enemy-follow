5fa0d562e6b9498d591b7ac154f36523460ae65a
lipo <85422792+thiliporace@users.noreply.github.com>
2023-02-22 04:36:41 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public NavMeshAgent enemy;
+    public Transform player;
+
+
+    private void Awake()
+    {
+        enemy = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>().transform;
+    }
+
+    void Update()
+    {
+        enemy.SetDestination(player.position);
+    }
+
+}
